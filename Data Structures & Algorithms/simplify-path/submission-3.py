8135dfb884b5ee5c44554eb9946b7416e3dff1cc
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-04-26 01:59:25 -0300

    Add: simplify-path - submission-3

---
+++ Data Structures & Algorithms/simplify-path/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+
+        ans = ""
+
+        i = 0
+        j = 0
+
+        while i < len(path):
+            if path[i] != "/":
+                j = i
+                while i < len(path) and path[i] != "/":
+                    i += 1
+
+                if path[j:i] == "..":
+                    if stack:
+                        stack.pop()
+                elif path[j:i] == ".":
+                    i += 1
+                    continue
+                else:
+                    stack.append(path[j:i])
+            else:
+                i += 1
+
+        ans = ""
+
+        for i in stack:
+            ans += "/"
+            ans += i
+
+        if stack:
+            return ans
+        else:
+            return "/"
